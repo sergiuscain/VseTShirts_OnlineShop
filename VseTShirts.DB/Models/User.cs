4d30084e0e8ffe3d8f821d7b7128e48072a56327
sergiuscain <sergiussmithe@gmail.com>
2024-10-21 16:15:27 +0300

    Добавил автомаппинг

--- VseTShirts.DB/Models/User.cs
+++ VseTShirts.DB/Models/User.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.Drawing;
 
 namespace VseTShirts.DB.Models
 {
     public class User : IdentityUser   
     {
-
+        public string Role { get; set; }
     }
 }
--- VseTShirts/Helpers/Helper.cs
+++ VseTShirts/Helpers/Helper.cs
@@ -1,6 +1,7 @@
 ﻿using VseTShirts.Areas.Admin.Models;
 using VseTShirts.DB.Models;
 using VseTShirts.Models;
+using AutoMapper;
 
 namespace VseTShirts;
 
@@ -22,61 +23,54 @@ public static class Helper
     }
     public static ProductViewModel ToViewModel(this Product product)
     {
-        return new ProductViewModel
-        {
-            Id = product.Id,
-            Name = product.Name,
-            Description = product.Description,
-            Price = product.Price,
-            Quantity = product.Quantity,
-            Sex = product.Sex,
-            ImagePath = product.ImagePath,
-            Category = product.Category,
-            Color = product.Color,
-            Size = product.Size,
-        };
+         MapperConfiguration mapperConfig = new MapperConfiguration(config => config.CreateMap<Product, ProductViewModel>());
+         Mapper mapper = new Mapper(mapperConfig);
+        return mapper.Map<Product, ProductViewModel>(product);
     }
 
 
     public static List<Product> ToDBModel(this List<ProductViewModel> list)
     {
-        var products = new List<Product>();
-        foreach (var product in list)
-        {
-            products.Add(ToDBModel(product));
-        }
-        return products;
+        MapperConfiguration mapperConfig = new MapperConfiguration(config => config.CreateMap<List<ProductViewModel>,List<Product>>());
+        Mapper mapper = new Mapper(mapperConfig);
+        return mapper.Map<List<ProductViewModel>, List<Product>>(list);
     }
     public static Product ToDBModel(this ProductViewModel productVM)
     {
-        return new Product
-        {
-            Id = productVM.Id,
-            Name = productVM.Name,
-            Description = productVM.Description,
-            Price = productVM.Price,
-            Quantity = productVM.Quantity,
-            Sex = productVM.Sex,
-            ImagePath = productVM.ImagePath,
-            Category = productVM.Category,
-            Color = productVM.Color,
-            Size = productVM.Size,
-        };
+        MapperConfiguration mapperConfig = new MapperConfiguration(config => config.CreateMap<ProductViewModel, Product>());
+        Mapper mapper = new Mapper(mapperConfig);
+        return mapper.Map < ProductViewModel, Product>(productVM);
+        //return new Product
+        //{
+        //    Id = productVM.Id,
+        //    Name = productVM.Name,
+        //    Description = productVM.Description,
+        //    Price = productVM.Price,
+        //    Quantity = productVM.Quantity,
+        //    Sex = productVM.Sex,
+        //    ImagePath = productVM.ImagePath,
+        //    Category = productVM.Category,
+        //    Color = productVM.Color,
+        //    Size = productVM.Size,
+        //};
     }
 
     public static OrderViewModel ToViewModel(this Order order)
     {
-        return new OrderViewModel
-        {
-            Id = order.Id,
-            Address = order.Address,
-            Name = order.Name,
-            Status = ToViewModel(order.Status),
-            Cart = new CartViewModel { UserId = order.UserId, Positions = order.Positions.Select(p => ToViewModel(p)).ToList()},
-            Phone = order.Phone,
-            DateAndTime = order.DateAndTime,
-            UserId = order.UserId
-        };
+        MapperConfiguration mapperConfig = new MapperConfiguration(config => config.CreateMap<Order, OrderViewModel>());
+        Mapper mapper = new Mapper(mapperConfig);
+        return mapper.Map<Order, OrderViewModel>(order);
+        //return new OrderViewModel
+        //{
+        //    Id = order.Id,
+        //    Address = order.Address,
+        //    Name = order.Name,
+        //    Status = ToViewModel(order.Status),
+        //    Cart = new CartViewModel { UserId = order.UserId, Positions = order.Positions.Select(p => ToViewModel(p)).ToList()},
+        //    Phone = order.Phone,
+        //    DateAndTime = order.DateAndTime,
+        //    UserId = order.UserId
+        //};
     }
     public static List<OrderViewModel> ToViewModel(this List<Order> orders)
     {
@@ -84,43 +78,43 @@ public static class Helper
     }
     public static Order ToDBModel(this OrderViewModel orderViewMOdel)
     {
-        return new Order
-        {
-            Id = orderViewMOdel.Id,
-            Address = orderViewMOdel.Address,
-            Name = orderViewMOdel.Name,
-            Status = ToDBModel(orderViewMOdel.Status),
-            Positions = ToDBModel(orderViewMOdel.Cart.Positions),
-            Phone = orderViewMOdel.Phone,
-            DateAndTime = orderViewMOdel.DateAndTime,
-            UserId = orderViewMOdel.UserId
-        };
+        MapperConfiguration mapperConfig = new MapperConfiguration(config => config.CreateMap<OrderViewModel, Order>());
+        Mapper mapper = new Mapper(mapperConfig);
+        return mapper.Map<OrderViewModel, Order>(orderViewMOdel);
+        //return new Order
+        //{
+        //    Id = orderViewMOdel.Id,
+        //    Address = orderViewMOdel.Address,
+        //    Name = orderViewMOdel.Name,
+        //    Status = ToDBModel(orderViewMOdel.Status),
+        //    Positions = ToDBModel(orderViewMOdel.Cart.Positions),
+        //    Phone = orderViewMOdel.Phone,
+        //    DateAndTime = orderViewMOdel.DateAndTime,
+        //    UserId = orderViewMOdel.UserId
+        //};
     }
 
 
     public static OrderStatusViewModel ToViewModel(this OrderStatus orderStatus)
     {
-        switch (orderStatus)
-        {
-            case OrderStatus.Создан: return OrderStatusViewModel.Создан;
-            case OrderStatus.Одобрен: return OrderStatusViewModel.Одобрен;
-            case OrderStatus.В_Доставке: return OrderStatusViewModel.В_Доставке;
-            case OrderStatus.Ожидает_Получения: return OrderStatusViewModel.Ожидает_Получения;
-            case OrderStatus.Получен: return OrderStatusViewModel.Получен;
-            default: return OrderStatusViewModel.Error;
-        }
+        MapperConfiguration mappingConfig = new MapperConfiguration(config => config.CreateMap<OrderStatus, OrderStatusViewModel>());
+        Mapper mapper = new Mapper(mappingConfig);
+        return mapper.Map<OrderStatus, OrderStatusViewModel>(orderStatus);
     }
     public static OrderStatus ToDBModel(this OrderStatusViewModel orderStatus)
     {
-        switch (orderStatus)
-        {
-            case OrderStatusViewModel.Создан: return OrderStatus.Создан;
-            case OrderStatusViewModel.Одобрен: return OrderStatus.Одобрен;
-            case OrderStatusViewModel.В_Доставке: return OrderStatus.В_Доставке;
-            case OrderStatusViewModel.Ожидает_Получения: return OrderStatus.Ожидает_Получения;
-            case OrderStatusViewModel.Получен: return OrderStatus.Получен;
-            default: return OrderStatus.Error;
-        }
+        MapperConfiguration mapperConfig = new MapperConfiguration(config => config.CreateMap<OrderStatusViewModel, OrderStatus>());
+        Mapper mapper = new Mapper(mapperConfig);
+        return mapper.Map<OrderStatusViewModel, OrderStatus>(orderStatus);
+        //switch (orderStatus)
+        //{
+        //    case OrderStatusViewModel.Создан: return OrderStatus.Создан;
+        //    case OrderStatusViewModel.Одобрен: return OrderStatus.Одобрен;
+        //    case OrderStatusViewModel.В_Доставке: return OrderStatus.В_Доставке;
+        //    case OrderStatusViewModel.Ожидает_Получения: return OrderStatus.Ожидает_Получения;
+        //    case OrderStatusViewModel.Получен: return OrderStatus.Получен;
+        //    default: return OrderStatus.Error;
+        //}
     }
     private static List<CartPosition> ToDBModel(this List<CartPositionViewModel> items)
     {
@@ -133,61 +127,78 @@ public static class Helper
     }
     public static CartPositionViewModel ToViewModel(this CartPosition cartPosition)
     {
-        return new CartPositionViewModel
-        {
-            Id = cartPosition.Id,
-            Name = cartPosition.Name,
-            Product = ToViewModel(cartPosition.Product),
-            Quantity = cartPosition.Quantity,
+        MapperConfiguration mapperConfig = new MapperConfiguration(config => config.CreateMap<CartPosition, CartPositionViewModel>());
+        Mapper mapper = new Mapper(mapperConfig);
+        return mapper.Map<CartPosition, CartPositionViewModel>(cartPosition);
+        //return new CartPositionViewModel
+        //{
+        //    Id = cartPosition.Id,
+        //    Name = cartPosition.Name,
+        //    Product = ToViewModel(cartPosition.Product),
+        //    Quantity = cartPosition.Quantity,
             
-        };
+        //};
     }
     public static CartPosition ToDBModel(this CartPositionViewModel cartPositionViewModel)
     {
-        return new CartPosition
-        {
-            Id = cartPositionViewModel.Id,
-            Name = cartPositionViewModel.Name,
-            Product = ToDBModel(cartPositionViewModel.Product),
-            Quantity = cartPositionViewModel.Quantity
-        };
+        MapperConfiguration mapperConfig = new MapperConfiguration(config => config.CreateMap<CartPositionViewModel, CartPosition>());
+        Mapper mapper = new Mapper(mapperConfig);
+        return mapper.Map<CartPositionViewModel, CartPosition>(cartPositionViewModel);
+        //return new CartPosition
+        //{
+        //    Id = cartPositionViewModel.Id,
+        //    Name = cartPositionViewModel.Name,
+        //    Product = ToDBModel(cartPositionViewModel.Product),
+        //    Quantity = cartPositionViewModel.Quantity
+        //};
     }
     public static CartViewModel ToViewModel(this Cart cartDB)
     {
-        if (cartDB == null)
-            return null;
-        CartViewModel newCart = new CartViewModel();
-        newCart.Id = cartDB.Id;
-        newCart.UserId = cartDB.UserId;
-        List<CartPositionViewModel> positions = new List<CartPositionViewModel>();
-        foreach (var position in cartDB.Positions)
-        {
-            positions.Add(ToViewModel(position));
-        }
-        newCart.Positions = positions;
-        return newCart;
+        MapperConfiguration mapperConfig = new MapperConfiguration(config => config.CreateMap<Cart,CartViewModel>());
+        Mapper mapper = new Mapper(mapperConfig);
+        return mapper.Map<Cart, CartViewModel>(cartDB);
+        //if (cartDB == null)
+        //    return null;
+        //CartViewModel newCart = new CartViewModel();
+        //newCart.Id = cartDB.Id;
+        //newCart.UserId = cartDB.UserId;
+        //List<CartPositionViewModel> positions = new List<CartPositionViewModel>();
+        //foreach (var position in cartDB.Positions)
+        //{
+        //    positions.Add(ToViewModel(position));
+        //}
+        //newCart.Positions = positions;
+        //return newCart;
     }
 
     public static Cart ToDBModel(this CartViewModel cartViewModel)
     {
-        Cart newCart = new Cart();
-        newCart.Id = cartViewModel.Id;
-        newCart.UserId = cartViewModel.UserId;
-        List<CartPosition> positions = new List<CartPosition>();
-        foreach (var position in cartViewModel.Positions)
-        {
-            positions.Add(ToDBModel(position));
-        }
-        newCart.Positions = positions;
-        return newCart;
+        MapperConfiguration mapperConfig = new MapperConfiguration(config => config.CreateMap<CartViewModel, Cart>());
+        Mapper mapper = new Mapper(mapperConfig);
+        return mapper.Map<CartViewModel, Cart>(cartViewModel);
+        //Cart newCart = new Cart();
+        //newCart.Id = cartViewModel.Id;
+        //newCart.UserId = cartViewModel.UserId;
+        //List<CartPosition> positions = new List<CartPosition>();
+        //foreach (var position in cartViewModel.Positions)
+        //{
+        //    positions.Add(ToDBModel(position));
+        //}
+        //newCart.Positions = positions;
+        //return newCart;
     }
     public static UserViewModel ToViewModel(this User user)
     {
-        return new UserViewModel()
-        {
-            Email = user.Email,
-            UserName = user.UserName,
-            PhoneNumber =  user.PhoneNumber
-        };
+        MapperConfiguration mapperConfig = new MapperConfiguration(config => config.CreateMap<User, UserViewModel>());
+        Mapper mapper = new Mapper(mapperConfig);
+        return mapper.Map<User, UserViewModel>(user);
+        //return new UserViewModel()
+        //{
+        //    Email = user.Email,
+        //    UserName = user.UserName,
+        //    PhoneNumber =  user.PhoneNumber,
+        //    Role = user.Role,
+
+        //};
     }
 }
