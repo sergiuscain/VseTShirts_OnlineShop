35e1aa89ea9b03d362f0b58ec1b31b92996625e7
sergiuscain <sergiussmithe@gmail.com>
2024-10-25 17:28:08 +0300

    Перевёл работу приложения обратно на MsSQL (С ней удобнее работать благодаря тому, что прям в VS Studio можно смотреть данные). Удалил изображения из файлов проекта

---
+++ VseTShirts.DB/Models/ProductImage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VseTShirts.DB.Models
+{
+    public class ProductImage
+    {
+        public Guid Id { get; set; }
+        public Guid ProductId { get; set; }
+        public Product Product { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
