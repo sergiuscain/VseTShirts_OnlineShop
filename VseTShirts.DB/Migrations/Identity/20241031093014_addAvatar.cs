3cf55a3ba248b9fc50a4c68a7d02baaf429f7e0d
sergiuscain <sergiussmithe@gmail.com>
2024-10-31 12:40:38 +0300

    Добавил аватарку

---
+++ VseTShirts.DB/Migrations/Identity/20241031093014_addAvatar.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VseTShirts.DB.Migrations.Identity
+{
+    /// <inheritdoc />
+    public partial class addAvatar : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AvatarURL",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AvatarURL",
+                table: "AspNetUsers");
+        }
+    }
+}
